fd45266d4d85332480ab45712c79962930d1b8ce
Beverly Lau <beverly.a.lau@gmail.com>
2015-07-24 11:13:49 -0700

    Fix user_id, add get username by id

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import requests
+import json
 from requests_oauthlib import OAuth1
 import secrets as s
+
+AUTH = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
 
 
 def run():
     url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
-    auth = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
-    r = requests.get(url, auth=auth)
+    r = requests.get(url, auth=AUTH)
     print(r.status_code)
 
 
 def list_by_username(username):
     url = 'https://api.twitter.com/1.1/friends/list.json?screen_name={username}'\
         .format(username=username)
-    auth = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
-    r = requests.get(url, auth=auth)
+    r = requests.get(url, auth=AUTH)
     print(r.status_code)
 
 
-def list_by_id(id):
-    url = 'https://api.twitter.com/1.1/friends/ids.json?id={id}'\
-        .format(id=id)
-    auth = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
-    r = requests.get(url, auth=auth)
+def list_by_id(user_id):
+    url = 'https://api.twitter.com/1.1/friends/ids.json?user_id={user_id}'\
+        .format(user_id=user_id)
+    r = requests.get(url, auth=AUTH)
+    print(r.status_code)
+
+
+def get_username_from_id(user_id):
+    url = 'https://api.twitter.com/1.1/users/lookup.json?user_id={user_id}'\
+        .format(user_id=user_id)
+    r = requests.get(url, auth=AUTH)
     print(r.status_code)
 
 if __name__ == "__main__":
-    list_by_username('JustinMoen')
+    get_username_from_id('20257992')
