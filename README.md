a21b390b6735da6ad92f9a315953dda5d22d0e59
Beverly Lau <beverly.a.lau@gmail.com>
2015-07-24 10:34:05 -0700

    Basic using-Twitter API, put secrets somewhere-not-on-Github

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This is an app to find people on Twitter.
+This is an app to find people on Twitter. 
+
+Installation:
+1. Create venv. Navigate to cloned directory, type this: 
+`python3.4 -m venv .` 
+
+2. Source venv: 
+`source bin/activate` 
+
+3. Install requirements: 
+`pip install -r requirements.txt`
+
+4. Do stuff.
+
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import requests
+from requests_oauthlib import OAuth1
+import secrets as s
 
 
 def run():
-    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+    auth = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
+    r = requests.get(url, auth=auth)
     print(r.status_code)
 
 
+def list_by_username(username):
+    url = 'https://api.twitter.com/1.1/friends/list.json?screen_name={username}'\
+        .format(username=username)
+    auth = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
+    r = requests.get(url, auth=auth)
+    print(r.status_code)
+
+
+def list_by_id(id):
+    url = 'https://api.twitter.com/1.1/friends/ids.json?id={id}'\
+        .format(id=id)
+    auth = OAuth1(s.API_KEY, s.API_SECRET, s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
+    r = requests.get(url, auth=auth)
+    print(r.status_code)
+
 if __name__ == "__main__":
-    run()
+    list_by_username('JustinMoen')
